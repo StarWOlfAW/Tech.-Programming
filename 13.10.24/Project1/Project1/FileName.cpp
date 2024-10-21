6e1c0455f72c7d4c5f1e9a7b364a9ead9a0ffa86
Evgeniy <143869955+StarWOlfAW@users.noreply.github.com>
2024-10-21 10:11:04 +0300

    Update FileName.cpp

--- 13.10.24/Project1/Project1/FileName.cpp
+++ 13.10.24/Project1/Project1/FileName.cpp
@@ -4,13 +4,13 @@
 int main() {
     long long number;
     using namespace std;
-    cout << "Введите число: ";
+    cout << "Enter numbers: ";
     cin >> number;
 
     int minDigit = 9, maxDigit = 0;
     vector<int> digits;
 
-    // Извлекаем цифры и находим минимальную и максимальную
+   
     while (number > 0) {
         int digit = number % 10;
         digits.push_back(digit);
@@ -19,9 +19,9 @@ int main() {
         number /= 10;
     }
 
-    // Выводим цифры между минимальной и максимальной
-    cout << "Цифры между минимальной и максимальной: ";
-    for (int i = digits.size() - 1; i >= 0; --i) { // Перебираем в обратном порядке
+   
+    c
+    for (int i = digits.size() - 1; i >= 0; --i) { 
         if (digits[i] > minDigit && digits[i] < maxDigit) {
             cout << digits[i];
         }
