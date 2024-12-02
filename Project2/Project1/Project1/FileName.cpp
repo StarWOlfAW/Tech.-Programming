4e5258e77a66d9e7cc5a7b401914dd3700858999
Evgeniy <143869955+StarWOlfAW@users.noreply.github.com>
2024-12-02 14:55:20 +0300

    Update FileName.cpp

--- Project2/Project1/Project1/FileName.cpp
+++ Project2/Project1/Project1/FileName.cpp
@@ -1,26 +1,96 @@
-#include <iostream>
-#include <cmath>
-
+#include <iostream>;
+#include <ctime>;
 using namespace std;
+int average(int* a, int n); int abovezero(int* a, int n); int abovezero2(int* a, int n); int coldest(int* a, int n); int process1(int* a, int n); void output_mass(int* a, int n); void inp_mass2(int* a, int n);
 int main()
 {
-	int a, a2 = 0, b = 0, s, b2, z;
-	cout << "Enter your number " << endl;
-	cin >> a;
-	b = a;
-	s = a;
-	b2 = a;
-	while (a)
-		{
-			a2++;
-			a = a / 10;
-			b = b * 10 + a % 10;
-		}
-	z = (b2 / (pow(10, a2) / 2));
-	if (((z%10) == 9) && (s == b)) {
-		cout << "Beautiful" << endl;
-	}
-	else cout << "Not so beautiful" << endl;
+	srand(time(0));
+	const int n = 31;
+	int* a = new int[n], i = 0;
+	inp_mass2(a, n);
+	output_mass(a, n);
+		//process(a, n);
+	cout << " >>> " << average(a, n) << " >1 " << abovezero(a, n) << " >2 " << abovezero2(a, n);
+	//output_mass(a, n);
 	system("pause");
 	return 0;
+
+}
+int process1(int* a, int n)
+{
+	int max = a[0];
+	for (int i = 0; i < n; i++)
+	{
+		if (a[i] > max)
+			max = a[i];
+	}
+	return max;
+}
+int average(int* a, int n)
+{
+	int sum = 0;
+	for (int i = 0; i < n; i++)
+		sum = a[i] + sum;
+	int average = sum / 31;
+	return average;
+}
+int abovezero(int* a, int n)
+{
+	int col = 0;
+	for (int i = 0; i < n; i++)
+	{
+		if (a[i] > 0)
+			col = col + 1;
+	}
+	return col;
+}
+int abovezero2(int* a, int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		if (a[i] > 0)
+		{
+			a[i]++;
+			break;
+			return i, a[i];
+		}
+		
+	}
+}
+int coldest(int* a, int n)
+{
+	int max = a[0];
+	for (int i = 0; i < n; i++)
+	{
+		if (a[i] < max)
+			max = a[i];
+	}
+	for (int i = 0; i < n; i++)
+	{
+		if (a[i] == max)
+			return i;
+	}
+}
+int daysaverage(int* a, int n)
+{
+	int sum = 0, s2 = 0;
+	for (int i = 0; i < n; i++)
+		sum = a[i] + sum;
+	int average = sum / 31;
+	for (int i = 0; i < n; i++)
+		if (a[i] > average)
+			s2++;
+	return s2;
+}
+void output_mass(int* a, int n)
+{
+	for (int i = 0; i < n; i++)
+		cout << *(a + i) << " ";
+}
+void inp_mass2(int* a, int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		a[i] = rand() % 31 - 20;
+	}
 }
