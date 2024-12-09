470634c0a876ffa10cc11a784a56b378c74a5559
Evgeniy <143869955+StarWOlfAW@users.noreply.github.com>
2024-12-09 14:54:58 +0300

    Update Source.cpp

--- 09.12.24Attest/Attest2N2/Attest2N2/Source.cpp
+++ 09.12.24Attest/Attest2N2/Attest2N2/Source.cpp
@@ -1,2 +1,13 @@
-
+#include "Header.h"
+#include <iostream>
+#include <ctime>
 using namespace std;
+int main()
+{
+	srand(time(0));
+	const int n = 100;
+	int* a = new int[n], i = 0;
+	int c = countsame(a, n);
+	int* b = new int[c];
+	inp_mass2(a, n);
+	output_mass(a, n);
