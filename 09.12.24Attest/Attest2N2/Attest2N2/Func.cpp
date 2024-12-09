6ee4477d4559f1a61673a28a040f93e330f610a5
Evgeniy <143869955+StarWOlfAW@users.noreply.github.com>
2024-12-09 14:54:19 +0300

    Update Func.cpp

--- 09.12.24Attest/Attest2N2/Attest2N2/Func.cpp
+++ 09.12.24Attest/Attest2N2/Attest2N2/Func.cpp
@@ -13,3 +13,22 @@ void inp_mass2(int* a, int n)
 		a[i] = ((rand() % 31) - 20);
 	}
 }
+void excludesame(int* a, int* b, int n)
+{
+
+}
+int countsame(int* a, int n)
+{
+	int sum = 0, c = 0;
+
+	for (int i = 0; i < n; i++)
+	{
+		int same = a[i];
+		for (int i = 1; i < n - 1; i++)
+		{
+			if (a[i] == same)
+				sum++;
+		}
+	}
+	return (n - c);
+}
