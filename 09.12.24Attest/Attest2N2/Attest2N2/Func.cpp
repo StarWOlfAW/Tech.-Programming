d7723d801e6dc0020866f584694678fd29f27c87
Evgeniy <143869955+StarWOlfAW@users.noreply.github.com>
2024-12-15 19:23:16 +0300

    Update Func(N2Attest2).cpp

--- 09.12.24Attest/Attest2N2/Attest2N2/Func.cpp
+++ 09.12.24Attest/Attest2N2/Attest2N2/Func.cpp
@@ -1,5 +1,6 @@
 #include "Header.h"
 #include <iostream>
+#include <ctime>
 using namespace std;
 void output_mass(int* a, int n)
 {
@@ -13,22 +14,39 @@ void inp_mass2(int* a, int n)
 		a[i] = ((rand() % 31) - 20);
 	}
 }
-void excludesame(int* a, int* b, int n)
+void excludesame(int* a,int n)
 {
-
+	for (int i = 0; i < n; i++)
+	{
+		int current = a[i];
+		for (int d = i; d < n - 1; d++)
+		{
+			if (a[d + 1] == current)
+				a[d + 1] = -23478956;
+		}
+	}
+}
+void typein(int* a, int* b, int c, int n)
+{
+	int r = 0;
+	for (int i = 0; i < n; i++)
+	{
+		if (a[i] != -23478956)
+		{
+			b[r] = a[i];
+			r++;
+		}
+	}
 }
 int countsame(int* a, int n)
 {
-	int sum = 0, c = 0;
-
-	for (int i = 0; i < n; i++)
-	{
-		int same = a[i];
-		for (int i = 1; i < n - 1; i++)
+	int sum = 0;
+		for (int i = 0; i < n; i++)
 		{
-			if (a[i] == same)
+			if (a[i] == -23478956)
 				sum++;
 		}
-	}
-	return (n - c);
+		int r;
+		r = n - sum;
+	return (r);
 }
