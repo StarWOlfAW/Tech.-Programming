bfdac921389c23ebe4d9d3369f27f053efefbf26
Evgeniy <143869955+StarWOlfAW@users.noreply.github.com>
2024-12-15 19:22:01 +0300

    Update Header.h

--- 09.12.24Attest/Attest2N2/Attest2N2/Header.h
+++ 09.12.24Attest/Attest2N2/Attest2N2/Header.h
@@ -1,7 +1,8 @@
-#ifndef Source
-#define Source
+#ifndef Main
+#define Main
 void output_mass(int* a, int n);
 void inp_mass2(int* a, int n);
-void excludesame(int* a, int* b, int n);
+void excludesame(int* a,int n);
 int countsame(int* a, int n);
+void typein(int* a, int* b, int c, int n);
 #endif
