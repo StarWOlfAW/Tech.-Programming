7478fca6f4b0189d92019cfc00aa569e4835a99a
Evgeniy <143869955+StarWOlfAW@users.noreply.github.com>
2024-12-15 19:22:22 +0300

    Update Source.cpp

--- 09.12.24Attest/Attest2N2/Attest2N2/Source.cpp
+++ 09.12.24Attest/Attest2N2/Attest2N2/Source.cpp
@@ -5,9 +5,18 @@ using namespace std;
 int main()
 {
 	srand(time(0));
-	const int n = 100;
+	const int n = 15;
 	int* a = new int[n], i = 0;
-	int c = countsame(a, n);
-	int* b = new int[c];
 	inp_mass2(a, n);
 	output_mass(a, n);
+	excludesame(a, n);
+	int c = countsame(a, n);
+	int* b = new int[c];
+	cout << " >>>>> " << c << "    ";
+	output_mass(a, n);
+	cout << "    ";
+	typein(a, b, c, n);
+	output_mass(b, c);
+	system("pause");
+	return 0;
+}
